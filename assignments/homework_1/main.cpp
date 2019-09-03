0d92b94b58bfe59fa0f35af7697ac0347c1c9dab
venetiaqueen <54634193+venetiaqueen@users.noreply.github.com>
2019-09-03 07:41:14 -0500

    Update main.cpp

--- assignments/homework_1/main.cpp
+++ assignments/homework_1/main.cpp
@@ -1,1 +1,86 @@
 
+
+#include <iostream> 
+#include <string>
+#include <ctime>     
+#include <fstream>  
+
+using namespace std; 
+
+#define SIZE 20
+
+
+
+
+struct Student
+{
+    string firstname; // first name of student
+    string lastname; // last name of student
+    int countGrades; // number of grades
+    int grades[20]; // size of grades
+};
+
+
+int loadClassList(Student *classList, string fileName) // function used to open the file and read the data
+{
+    ifstream fin;   //readin   
+    fin.open(fileName); // open the stream using fileName parameter
+    int i = 0;          // index (counter)
+
+
+    while (!fin.eof())
+    { 
+        fin >> classList[i].firstname >> classList[i].lastname >> classList[i].countGrades; //readin students' first name, lastname and no. of grades
+
+        
+  
+for(int x = 0; x < classList[i].countGrades; x++) // 
+{
+
+      
+      fin >> classList[i].grades[x];
+}
+  i++; // return count of grades
+    }
+    return i;
+}
+
+
+void printClassList(Student *classList, int classSize)
+{
+  ofstream fout;
+  fout.open("student_output.dat");
+  fout << "Students" << endl;
+  fout << "==================================="<< endl;
+  
+    for (int i = 0; i < classSize; i++)
+    {
+        fout <<i+1<<". "
+             << classList[i].firstname << " "
+             << classList[i].lastname << ":\t";
+    for ( int x = 0; x < classList[i].countGrades; x++)
+    {
+            fout << classList[i].grades[x] << "  ";
+
+    }
+
+    fout<<endl;
+}
+fout.close();
+}
+
+
+int main()
+{
+  
+     
+    Student classList[100];
+    int classSize = 0;
+
+    classSize = loadClassList(classList, "input_data.txt");
+
+    printClassList(classList, classSize);
+
+
+    return 0;
+} 
