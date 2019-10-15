5de506963489dd9b6cc8052e3d6486971ea4f700
Faith Howell <54634193+venetiaqueen@users.noreply.github.com>
2019-10-15 10:58:21 -0500

    Create ListStack.h

---
+++ assignments/homework_03/ListStack.h
@@ -0,0 +1,70 @@
+/**
+ * ListStack.h
+ * 
+ * List stack definition file. This stack hold animal types. Why? Cause integers
+ * get a little boring after a while. 
+ * 
+ * Author: Faith Howell
+ * Date: Oct 14th 2019
+ */
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Animal Type
+ */
+struct Animal {
+    string name;
+    double weight;
+    double scary;
+
+    // Default constructor
+    Animal() {
+        name = "";
+        weight = 0.0;
+        scary = 99.0;
+    }
+
+    // Should add an overloaded constructor
+    Animal(string n, double w, double s);
+
+};
+
+// Overload "<<" operator for animals
+ostream &operator<<(ostream &, const Animal *);
+
+/**
+ * Node type.
+ * 
+ * We seperate Animal from Node so we can return an "Animal"
+ * when we pop the stack, and don't have to return a "node"
+ */
+struct Node {
+    Animal *A;
+    Node *Next;
+
+    // Needs a default constructor
+
+    Node(Animal *a) {
+        A = a;
+        Next = NULL;
+    }
+};
+
+/**
+ * ListStack
+ * 
+ * List based stack implementation
+ * 
+ */
+class ListStack {
+private:
+    Node *Top;
+
+public:
+    ListStack();          // constructor
+    void Push(Animal *);  // stack gets Animals
+    Animal *Pop();        // Pop returns top animal
+    void Print();         // print stack for debugging
+};
